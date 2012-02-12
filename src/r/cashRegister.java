e7ee649f0da304769269e8c0a406dacdbbab7dcf
Kevin Kelley <kkelley1987@gmail.com>
2012-02-12 13:54:17 -0600

    program done, final commit

--- src/r/cashRegister.java
+++ src/r/cashRegister.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 import java.util.Vector;
 
 
